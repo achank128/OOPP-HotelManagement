2fe22418154e4fe87de3e9828bf28e8d83d28f83
NKC <khacchang128@gmail.com>
2022-04-30 22:35:21 +0700

    Add file model, view

--- src/controller/DAO.java
+++ src/controller/DAO.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 
-/**
- *
- * @author Than
- */
 public class DAO {
     private Connection conn;
     
